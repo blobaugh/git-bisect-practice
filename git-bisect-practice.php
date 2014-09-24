91936cb04f8cfc785bf61ec02c12c45d908a904b
blobaugh <ben@lobaugh.net>
2014-09-24 07:12:31 -0700

    Setup basic shortcode

--- git-bisect-practice.php
+++ git-bisect-practice.php
@@ -3,3 +3,15 @@
  * Plugin Name: Git Bisect Practice
  * Description: A simple shortcode that adds two numbers together for display. But with an error....you debug
  **/
+
+function add_adding_shortcode( $atts ) {
+      $atts = shortcode_atts( array(
+ 	      'a' => 0,
+ 	      'b' => 0
+      ), $atts );
+
+	$total = $atts['a'] + $atts['b'];
+
+      return "$total";
+}
+add_shortcode( 'add', 'add_adding_shortcode' );
